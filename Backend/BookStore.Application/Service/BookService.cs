3a7b1079417b2dc7b6de3dda6aae49ad7980b414
Danil Kiikov <49486980+ReadGee@users.noreply.github.com>
2024-08-02 18:20:21 +0300

    backend is ready
    backend is ready;
    The project is divided into two parts: frontend and backend

---
+++ Backend/BookStore.Application/Service/BookService.cs
@@ -0,0 +1,34 @@
+﻿using BookStore.Core.Models;
+using BookStore.DataAccess.Repository;
+
+namespace BookStore.Application.Service
+{
+    public class BookService : IBookService
+    {
+        private readonly IBooksRepository _booksRepository;
+        public BookService(IBooksRepository booksRepository)
+        {
+            _booksRepository = booksRepository;
+        }
+
+        public async Task<List<Book>> GetAllBook()
+        {
+            return await _booksRepository.Get();
+        }
+
+        public async Task<Guid> CreateBook(Book book)
+        {
+            return await _booksRepository.Create(book);
+        }
+
+        public async Task<Guid> UpdateBook(Guid id, string title, string descriptions, decimal price)
+        {
+            return await (_booksRepository.Update(id, title, descriptions, price));
+        }
+
+        public async Task<Guid> DeleteBook(Guid id)
+        {
+            return await _booksRepository.Delete(id);
+        }
+    }
+}
---
+++ Backend/BookStore.Core/Abstraction/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using BookStore.Core.Models;
+
+namespace BookStore.Application.Service
+{
+    public interface IBookService
+    {
+        Task<Guid> CreateBook(Book book);
+        Task<Guid> DeleteBook(Guid id);
+        Task<List<Book>> GetAllBook();
+        Task<Guid> UpdateBook(Guid id, string title, string descriptions, decimal price);
+    }
+}
